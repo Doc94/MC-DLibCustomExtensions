fcc5244ab9d884e4f29abfc64fe8bb8f6871a0aa
Doc <nachito94@msn.com>
2025-06-05 16:05:03 -0400

    feat(items): improvements in helper events

--- src/main/java/dev/mrdoc/minecraft/dlibcustomextension/items/classes/AbstractCustomItem.java
+++ src/main/java/dev/mrdoc/minecraft/dlibcustomextension/items/classes/AbstractCustomItem.java
@@ -33,25 +33,64 @@ public abstract non-sealed class AbstractCustomItem extends AbstractBaseCustomIt
     }
 
     /**
-     * This fired when the Material of item match (you need add more conditionals for any cases)
+     * Handles the event when a player consumes an item. This method is triggered when a player
+     * in the game consumes/uses a consumable item (like food, potions, etc.) that matches the
+     * item type. Implement additional conditions and logic within this method to handle specific
+     * behaviors related to item consumption.
+     *
+     * @param event the PlayerItemConsumeEvent triggered when a player consumes an item
      */
-    protected abstract void onConsumeMaterialItem(PlayerItemConsumeEvent event);
+    protected abstract void onPlayerConsumeItem(PlayerItemConsumeEvent event);
 
+    /**
+     * Handles the event when a player drops an item. This method is triggered whenever a player
+     * in the game drops an item from their inventory. Implement additional conditions and logic
+     * within this method to handle specific cases or behaviors related to item drop events.
+     *
+     * @param event the PlayerDropItemEvent triggered when a player drops an item
+     */
     protected abstract void onPlayerDropItem(PlayerDropItemEvent event);
 
+    /**
+     * Handles the event when an entity picks up an item. This method is triggered when an entity in the game world
+     * interacts with an item by picking it up. Implement additional logic within this method to handle specific
+     * behaviors or conditions related to item pickup events.
+     *
+     * @param event the EntityPickupItemEvent triggered when an entity picks up an item
+     */
     protected abstract void onEntityPickupItem(EntityPickupItemEvent event);
 
-    public abstract void onPlayerInteract(PlayerInteractEvent event);
+    /**
+     * Handles player interaction events. This method is triggered when a player interacts
+     * with an object in the game world, such as blocks or items. It is designed to handle
+     * specific conditions and behaviors based on the interaction context and associated logic.
+     *
+     * @param event the PlayerInteractEvent triggered when a player interacts with an object
+     */
+    protected abstract void onPlayerInteract(PlayerInteractEvent event);
 
+    /**
+     * Handles the player's interaction with an entity. This method is triggered when a player right-clicks or interacts
+     * with an entity while holding an item of the matching type. Additional conditions may need to be implemented
+     * based on specific use cases to handle interactions appropriately.
+     *
+     * @param event the PlayerInteractEntityEvent triggered when a player interacts with an entity
+     */
     protected abstract void onPlayerInteractEntity(PlayerInteractEntityEvent event);
 
-    public abstract void onItemReplace(PlayerDropItemEvent event, ItemStack itemReplace);
+    /**
+     * Call when the item was replaced by {@link PlayerDropItemEvent}
+     *
+     * @param event event
+     * @param itemReplace new item
+     */
+    protected abstract void onItemReplace(PlayerDropItemEvent event, ItemStack itemReplace);
 
     @EventHandler
     public void playerInteractEntity(PlayerInteractEntityEvent event) {
         if (event.getHand().toString().contains("HAND")) {
             if (this.isItem(event.getPlayer().getInventory().getItemInMainHand())) {
-                onPlayerInteractEntity(event);
+                this.onPlayerInteractEntity(event);
             }
         }
     }
@@ -60,7 +99,7 @@ public abstract non-sealed class AbstractCustomItem extends AbstractBaseCustomIt
     public void playerInteract(PlayerInteractEvent event) {
         if (!event.getAction().equals(Action.PHYSICAL)) {
             if (event.getItem() != null && this.isItem(event.getItem())) {
-                onPlayerInteract(event);
+                this.onPlayerInteract(event);
             }
         }
     }
@@ -68,7 +107,7 @@ public abstract non-sealed class AbstractCustomItem extends AbstractBaseCustomIt
     @EventHandler
     public void entityPickupItem(EntityPickupItemEvent event) {
         if (this.isItem(event.getItem().getItemStack())) {
-            onEntityPickupItem(event);
+            this.onEntityPickupItem(event);
         }
     }
 
@@ -86,11 +125,11 @@ public abstract non-sealed class AbstractCustomItem extends AbstractBaseCustomIt
             }
 
             event.getItemDrop().setItemStack(itemReplace); // Replace Item
-            onItemReplace(event, itemReplace);
+            this.onItemReplace(event, itemReplace);
             if (event.isCancelled()) {
                 return;
             }
-            onPlayerDropItem(event);
+            this.onPlayerDropItem(event);
         }
     }
 
@@ -99,7 +138,7 @@ public abstract non-sealed class AbstractCustomItem extends AbstractBaseCustomIt
         ItemStack itemConsume = event.getItem();
 
         if (this.isItem(itemConsume)) {
-            onConsumeMaterialItem(event);
+            this.onPlayerConsumeItem(event);
         }
 
     }
